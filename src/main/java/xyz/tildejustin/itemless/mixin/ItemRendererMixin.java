500b31744c63b4da37a79aeb6a5a36e37c7a7bfa
tildejustin <spacepepper3.14@gmail.com>
2023-06-09 21:26:39 -0400

    another justin one mixin special

---
+++ src/main/java/xyz/tildejustin/itemless/mixin/ItemRendererMixin.java
@@ -0,0 +1,76 @@
+package xyz.tildejustin.itemless.mixin;
+
+import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.item.ItemStack;
+import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(ItemRenderer.class)
+public abstract class ItemRendererMixin {
+    private static final String ItemStack = "net/minecraft/class_1071";
+    private static final String count = "field_4376";
+    private static final String getCount = "method_13652";
+    private static final String isEmpty = "method_13654";
+    private static final String ItemStack$count = "L" + ItemStack + ";" + count + ":I";
+    private static final String ItemStack$getCount = "L" + ItemStack + ";" + getCount + "()I";
+    private static final String ItemStack$isEmpty = "L" + ItemStack + ";" + isEmpty + "()I";
+
+
+    // method_1549 = 1.3.x-1.4.x
+    // method_5178 = 1.5.x-1.7.x
+    // method_10228 (renderGuiItemOverlay) = 1.8+
+    // 1.8.0-1.12.0 already checks ItemStack.count != 1 instead of > 1 so the only change the mod makes is
+    // to render 1 stacks, which is the most :tf: mod that could ever be made
+    // 1.12.1+ checks ItemStack$isEmpty as a anti-conditional for renderGuiItemOverlay,
+    // which returns true if ItemStack$count < 1, so to render negative numbers that is forced to return false
+    // It also calls ItemStack$getCount instead of accessing a field, b/c it's quirky like that
+
+
+    // 1.3.0-1.12.0 support
+    @Redirect(
+            method = {"method_1549", "method_5178", "method_10228"},
+            at = @At(
+                    value = "FIELD",
+                    target = ItemStack$count,
+                    opcode = Opcodes.GETFIELD,
+                    ordinal = 0,
+                    remap = false
+            ),
+            remap = false
+    )
+    private int itemless$alwaysRenderItemString(ItemStack itemStack) {
+        return 2; // 2 > 1
+    }
+
+    // 1.12.1+ support
+    @Redirect(
+            method = "method_10228",
+            at = @At(
+                    value = "INVOKE",
+                    target = ItemStack$getCount,
+                    ordinal = 0,
+                    remap = false
+            ),
+            remap = false
+    )
+    private int itemless$alwaysRenderItemStringOneTwelve(ItemStack itemStack) {
+        return 2;
+    }
+
+    // 1.12.1+ support
+    @Redirect(
+            method = "method_10228",
+            at = @At(
+                    value = "INVOKE",
+                    target = ItemStack$isEmpty,
+                    ordinal = 0,
+                    remap = false
+            ),
+            remap = false
+    )
+    private boolean itemless$lieAboutNotBeingEmpty() {
+        return false;
+    }
+}
